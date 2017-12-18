0709f121df792e7561d59391030885518803987a
llan <llan@amwares.com>
2017-12-18 17:42:55 +0800

    add move shape && add move rule

--- src/components/MainScreen.vue
+++ src/components/MainScreen.vue
@@ -1,9 +1,20 @@
 <template>
   <div class="canvas">
-    <Shape
+    <div v-for="arrObj of arrs">
+      <Shape
       :keyCode="keyCode"
       v-on:removeKeyCode="handleRemoveKeyCode"
-      :arrs="arrs"/>
+      v-on:init="init"
+      v-on:upDateArea="upDateArea"
+      :area="area"
+      :count="count"
+      :arr="arrObj.arr"
+      :active="arrObj.active"
+      :initLeft.sync="arrObj.left"
+      :initTop.sync="arrObj.top"
+      :parentRect="rect"
+      :fontSize="fontSize"/>
+    </div>
   </div>
 </template>
 
@@ -18,15 +29,33 @@ export default {
   data: function(){
     return {
       keyCode: null,
-      arrs: [
-        [1,0],
-        [1,0],
-        [1,1]
-      ]
+      arrs: [],
+      area:[],
+      fontSize:0,
+      rect:{},
+      count:0
     }
   },
   created: function () {
     window.addEventListener('keydown',this.handleKeyUp);
+    this.init();
+  },
+  mounted: function(){
+    const rect = this.$el.getBoundingClientRect();
+    const {width,height} = rect;
+    const fontSize = parseFloat(window.getComputedStyle(this.$el, null).getPropertyValue('font-size'));
+    const rowLength = parseFloat(width / fontSize /this.GLOBAL.offset);
+    const colLength = parseFloat(height / fontSize / this.GLOBAL.offset);
+    let arr = [];
+    for(let i = 0;i < colLength;i++){
+      arr[i] = new Array;
+      for(let j = 0;j < rowLength;j++){
+        arr[i][j] = 0;
+      }
+    }
+    this.area = arr;
+    this.rect = {width,height};
+    this.fontSize = fontSize;
   },
   methods: {
     handleKeyUp: function (event) {
@@ -34,8 +63,35 @@ export default {
         this.keyCode = event.keyCode;
       }
     },
-    handleRemoveKeyCode: function (){
+    handleRemoveKeyCode: function () {
       this.keyCode = null;
+    },
+    init: function () {
+      const random = Math.floor(Math.random() * this.GLOBAL.arrs.length);
+      const arr = this.GLOBAL.arrs[random];
+      const arrObj = {
+        active: true,
+        arr: arr,
+        left: 0,
+        top: 0
+      };
+      const length = this.arrs.length;
+      if(length){
+        this.arrs[length -1].active = false;
+        this.count ++;
+      }
+      this.arrs.push(arrObj);
+    },
+    upDateArea: function() {
+      let {arr,left,top} = this.arrs[this.count];
+      for(let i = 0;i < arr.length;i++){
+        for(let j = 0;j< arr[0].length;j++){
+          if(arr[i][j] === 1){
+            this.area[i+top][j+left] = 1;
+          }
+        }
+      }
+      console.log('this.area',this.area)
     }
   }
 }
--- src/components/Shape.vue
+++ src/components/Shape.vue
@@ -19,14 +19,25 @@ export default {
   },
   props: {
     keyCode: Number,
-    arrs: Array
+    arr: Array,
+    active: Boolean,
+    area: Array,
+    parentRect:Object,
+    fontSize:Number,
+    initLeft:Number,
+    initTop:Number,
+    count: Number
   },
   data: function () {
     return {
-      left: 0,
-      top: 0,
-      blockArrs: this.arrs
+      left: this.initLeft,
+      top: this.initTop,
+      blockArrs: this.arr,
+      isActive: this.active
     }
+  },
+  created: function (){
+    this.fallDown();
   },
   computed: {
     positionObject: function () {
@@ -36,12 +47,6 @@ export default {
         left:`${left}em`,
         top:`${top}em`
       }
-    },
-    parentRect: function () {
-      return this.$parent.$el.getBoundingClientRect();
-    },
-    fontSize: function () {
-      return parseFloat(window.getComputedStyle(this.$parent.$el, null).getPropertyValue('font-size'));
     },
     width: function () {
       return this.blockArrs[0].length;
@@ -56,14 +61,91 @@ export default {
       return Object.keys(revBlockArrs[0]).map(function(c) {
         return revBlockArrs.map(function(r) { return r[c]; });
       });
+    },
+    canMove: function (direction) {
+      let canMove = true;
+      if(direction === 'left'){
+        if(this.count){
+          if(this.left - 1 < 0){
+            return canMove = false;
+          }
+          const blockArrs = this.blockArrs;
+            for(let i = 0;i < blockArrs.length;i++){
+              if(!canMove) break;
+              for(let j = 0;j< blockArrs[0].length;j++){
+                  if(this.area[i+this.top][j+this.left-1] == 1){
+                      canMove = false;
+                      break;
+                  }
+              }
+            }
+        }else{
+          canMove = this.left - 1 >= 0;
+        }
+      }else if(direction === 'right'){
+        if(this.count){
+          if(this.left + this.width + 1 > this.area[0].length){
+            return canMove = false;
+          }
+          const blockArrs = this.blockArrs;
+            for(let i = 0;i < blockArrs.length;i++){
+              if(!canMove) break;
+              for(let j = 0;j< blockArrs[0].length;j++){
+                  if(this.area[i+this.top][j+this.left+1] == 1){
+                      canMove = false;
+                      break;
+                  }
+              }
+            }
+        }else{
+          canMove = this.left + this.width + 1 <= this.area[0].length;
+        }
+      }else if(direction === 'down'){
+        if(this.count){
+          if(this.top + this.height + 1 > this.area.length){
+            return canMove = false;
+          }
+          for(let i = 0;i < this.blockArrs.length;i++){
+            for(let j = 0;j< this.blockArrs[0].length;j++){
+                if(this.area[i+this.top+1][j+this.left] === 1){
+                    return canMove = false;
+                }
+            }
+          }
+        }else{
+          canMove = this.top + this.height + 1 <= this.area.length;
+        }
+      }else{
+        console.log('error direction');
+      }
+      return canMove;
+    },
+    fallDown: function () {
+      if(this.isActive){
+          setTimeout(function loop() {
+          if(this.canMove('down')){
+            this.top++;
+            setTimeout(loop.bind(this), 600);
+          }else{
+            console.log('can not fallDown!');
+            this.isActive = false;
+            clearTimeout(this.fallDown);
+            this.$emit('update:initLeft', this.left);
+            this.$emit('update:initTop', this.top);
+            this.$emit('upDateArea');
+            this.$emit('init');
+          }
+        }.bind(this), 600);
+      }
     }
   },
   watch: {
     keyCode: function (newKeyCode){
-      switch (newKeyCode) {
+      if(this.isActive){
+        switch (newKeyCode) {
         //left
         case 37:
-          if(this.left - 1 >= 0){
+          if(this.canMove('left')){
             this.left -= 1;
           }
           this.$emit('removeKeyCode');
@@ -77,14 +159,14 @@ export default {
           break;
         //right
         case 39:
-          if((this.left + this.width + 1) * this.GLOBAL.offset * this.fontSize <= this.parentRect.width){
+          if(this.canMove('right')){
             this.left += 1;
           }
           this.$emit('removeKeyCode');
           break;
         //down
         case 40:
-          if((this.top + this.height + 1 ) * this.GLOBAL.offset * this.fontSize <= this.parentRect.height){
+          if(this.canMove('down')){
             this.top += 1;
           }
           this.$emit('removeKeyCode');
@@ -93,6 +175,7 @@ export default {
           this.$emit('removeKeyCode');
       }
     }
+      }
   }
 }
 </script>
